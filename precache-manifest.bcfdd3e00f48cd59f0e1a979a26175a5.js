493a6d1706cc8e40c3278bfb4fbd18b0c66a915d
Alexandra Koren <korena@uw.edu>
2020-06-04 22:36:37 -0700

    Updates

---
+++ precache-manifest.bcfdd3e00f48cd59f0e1a979a26175a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "487d575d24bdd7e18bdaf3f382197bb9",
+    "url": "/final-project/index.html"
+  },
+  {
+    "revision": "f350e91bf0f8a5057104",
+    "url": "/final-project/static/css/main.ccd84271.chunk.css"
+  },
+  {
+    "revision": "f8bf9695202bb2578da9",
+    "url": "/final-project/static/js/2.917db1aa.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/final-project/static/js/2.917db1aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f350e91bf0f8a5057104",
+    "url": "/final-project/static/js/main.418ca8dc.chunk.js"
+  },
+  {
+    "revision": "3b459cb458b4cc682c58",
+    "url": "/final-project/static/js/runtime-main.9eb770de.js"
+  }
+]);
