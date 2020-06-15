bf6ca9c3208743dcdfec1b0cfc3772da1f51d533
Alexandra Koren <korena@uw.edu>
2020-06-15 16:13:52 -0700

    Updates

---
+++ precache-manifest.24285d2585572c4947849ced443ddbfd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19fd19be3ce2ba9adfc4bdbb77782fde",
+    "url": "/final-project/index.html"
+  },
+  {
+    "revision": "23eac39ffbcba9133fc0",
+    "url": "/final-project/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5b5641b67169b0038d83",
+    "url": "/final-project/static/css/main.55110eda.chunk.css"
+  },
+  {
+    "revision": "23eac39ffbcba9133fc0",
+    "url": "/final-project/static/js/2.ca7553d3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/final-project/static/js/2.ca7553d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b5641b67169b0038d83",
+    "url": "/final-project/static/js/main.973e71ec.chunk.js"
+  },
+  {
+    "revision": "3b459cb458b4cc682c58",
+    "url": "/final-project/static/js/runtime-main.9eb770de.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/final-project/precache-manifest.bcfdd3e00f48cd59f0e1a979a26175a5.js"
+  "/final-project/precache-manifest.24285d2585572c4947849ced443ddbfd.js"
 );
 
 self.addEventListener('message', (event) => {
